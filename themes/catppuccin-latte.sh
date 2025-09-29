91660981fc188bb4d7980bc233772f8b4091ed4a
abstrucked <abstrucked@users.noreply.github.com>
2025-09-29 11:26:40 +0200

    feat: add support for all Catppuccin flavors (latte, frappe, macchiato)
    - Add theme files for catppuccin-latte, catppuccin-frappe, and catppuccin-macchiato
    - Update README with theme switching instructions and project structure
    - Theme system now supports switching between all 4 Catppuccin flavors

---
+++ themes/catppuccin-latte.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# Catppuccin Latte theme color definitions
+# Extracted from alacritty catppuccin-latte.toml
+
+export THEME_NAME="catppuccin-latte"
+export THEME_FLAVOUR="latte"
+
+# Primary colors
+export PRIMARY_BACKGROUND="#eff1f5"
+export PRIMARY_FOREGROUND="#4c4f69"
+export PRIMARY_DIM_FOREGROUND="#8c8fa1"
+export PRIMARY_BRIGHT_FOREGROUND="#4c4f69"
+
+# Cursor
+export CURSOR_TEXT="#eff1f5"
+export CURSOR_CURSOR="#dc8a78"
+
+# Vi mode cursor
+export VI_MODE_CURSOR_TEXT="#eff1f5"
+export VI_MODE_CURSOR_CURSOR="#7287fd"
+
+# Search
+export SEARCH_MATCHES_FOREGROUND="#eff1f5"
+export SEARCH_MATCHES_BACKGROUND="#6c6f85"
+export SEARCH_FOCUSED_MATCH_FOREGROUND="#eff1f5"
+export SEARCH_FOCUSED_MATCH_BACKGROUND="#40a02b"
+
+# Footer bar
+export FOOTER_BAR_FOREGROUND="#eff1f5"
+export FOOTER_BAR_BACKGROUND="#6c6f85"
+
+# Hints
+export HINTS_START_FOREGROUND="#eff1f5"
+export HINTS_START_BACKGROUND="#df8e1d"
+export HINTS_END_FOREGROUND="#eff1f5"
+export HINTS_END_BACKGROUND="#6c6f85"
+
+# Selection
+export SELECTION_TEXT="#eff1f5"
+export SELECTION_BACKGROUND="#dc8a78"
+
+# Normal colors
+export NORMAL_BLACK="#bcc0cc"
+export NORMAL_RED="#d20f39"
+export NORMAL_GREEN="#40a02b"
+export NORMAL_YELLOW="#df8e1d"
+export NORMAL_BLUE="#1e66f5"
+export NORMAL_MAGENTA="#ea76cb"
+export NORMAL_CYAN="#179299"
+export NORMAL_WHITE="#5c5f77"
+
+# Bright colors
+export BRIGHT_BLACK="#acb0be"
+export BRIGHT_RED="#d20f39"
+export BRIGHT_GREEN="#40a02b"
+export BRIGHT_YELLOW="#df8e1d"
+export BRIGHT_BLUE="#1e66f5"
+export BRIGHT_MAGENTA="#ea76cb"
+export BRIGHT_CYAN="#179299"
+export BRIGHT_WHITE="#6c6f85"
+
+# Indexed colors
+export INDEXED_16="#fe640b"
+export INDEXED_17="#dc8a78"
---
+++ themes/catppuccin-macchiato.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# Catppuccin Macchiato theme color definitions
+# Extracted from alacritty catppuccin-macchiato.toml
+
+export THEME_NAME="catppuccin-macchiato"
+export THEME_FLAVOUR="macchiato"
+
+# Primary colors
+export PRIMARY_BACKGROUND="#24273a"
+export PRIMARY_FOREGROUND="#cad3f5"
+export PRIMARY_DIM_FOREGROUND="#8087a2"
+export PRIMARY_BRIGHT_FOREGROUND="#cad3f5"
+
+# Cursor
+export CURSOR_TEXT="#24273a"
+export CURSOR_CURSOR="#f4dbd6"
+
+# Vi mode cursor
+export VI_MODE_CURSOR_TEXT="#24273a"
+export VI_MODE_CURSOR_CURSOR="#b7bdf8"
+
+# Search
+export SEARCH_MATCHES_FOREGROUND="#24273a"
+export SEARCH_MATCHES_BACKGROUND="#a5adcb"
+export SEARCH_FOCUSED_MATCH_FOREGROUND="#24273a"
+export SEARCH_FOCUSED_MATCH_BACKGROUND="#a6da95"
+
+# Footer bar
+export FOOTER_BAR_FOREGROUND="#24273a"
+export FOOTER_BAR_BACKGROUND="#a5adcb"
+
+# Hints
+export HINTS_START_FOREGROUND="#24273a"
+export HINTS_START_BACKGROUND="#eed49f"
+export HINTS_END_FOREGROUND="#24273a"
+export HINTS_END_BACKGROUND="#a5adcb"
+
+# Selection
+export SELECTION_TEXT="#24273a"
+export SELECTION_BACKGROUND="#f4dbd6"
+
+# Normal colors
+export NORMAL_BLACK="#494d64"
+export NORMAL_RED="#ed8796"
+export NORMAL_GREEN="#a6da95"
+export NORMAL_YELLOW="#eed49f"
+export NORMAL_BLUE="#8aadf4"
+export NORMAL_MAGENTA="#f5bde6"
+export NORMAL_CYAN="#8bd5ca"
+export NORMAL_WHITE="#b8c0e0"
+
+# Bright colors
+export BRIGHT_BLACK="#5b6078"
+export BRIGHT_RED="#ed8796"
+export BRIGHT_GREEN="#a6da95"
+export BRIGHT_YELLOW="#eed49f"
+export BRIGHT_BLUE="#8aadf4"
+export BRIGHT_MAGENTA="#f5bde6"
+export BRIGHT_CYAN="#8bd5ca"
+export BRIGHT_WHITE="#a5adcb"
+
+# Indexed colors
+export INDEXED_16="#f5a97f"
+export INDEXED_17="#f4dbd6"
