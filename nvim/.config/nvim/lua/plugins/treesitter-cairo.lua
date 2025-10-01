f08aa5f01a731085cf73ec419a199640df5c814d
abstrucked <mxhgraphics@gmail.com>
2025-10-01 07:23:57 +0200

    chore: remove cairo plugin and related files

--- nvim/.config/nvim/lua/plugins/treesitter-cairo.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    opts.ensure_installed = opts.ensure_installed or {}
-    -- If a maintained grammar exists, e.g. "cairo":
-    -- table.insert(opts.ensure_installed, "cairo")
-    return opts
-  end,
-}
