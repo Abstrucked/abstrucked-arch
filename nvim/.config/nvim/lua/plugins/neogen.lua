5d024d8aac106725a9bffd9289a2b0243d699552
abstrucked <abstrucked@users.noreply.github.com>
2025-10-01 07:22:10 +0200

    feat: add mini.pick plugin and fix neogen to use normal install

--- nvim/.config/nvim/lua/plugins/neogen.lua
+++ nvim/.config/nvim/lua/plugins/neogen.lua
@@ -1,7 +1,5 @@
 return {
   "danymat/neogen",
-  dev = true,
-  dir = "~/_dev/neogen", -- Full path to your local clone
   config = function()
     require("neogen").setup({
       -- your config here
